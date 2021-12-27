f2acd8088ccc696fb7859d7fb2d3240ab9e15188
DominikaPietrzyk <72759164+DominikaPietrzyk@users.noreply.github.com>
2021-12-27 15:04:29 +0100

    update landing page

--- sites/index.php
+++ sites/index.php
@@ -7,13 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="/styles/styles.css">
+    <link rel="stylesheet" href="/styles//styles.css">
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+    <script src="https://kit.fontawesome.com/d747795ef6.js" crossorigin="anonymous"></script>
     <title>Jedzeniowo</title>
 </head>
+<style>
+    <?php include "/styles/styles.css" ?>
+</style>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-custom" style="padding-top: 15px;">
@@ -51,37 +55,96 @@
     <div class="nav-image">
         <div class="container">
             <div class="row justify-content-end">
-                <h1 class="landing-page-text">Ekskluzywny smak <br> europejskiej kuchni !</h1>
+                <h1 class="landing-page-text ml-4">Ekskluzywny smak <br> europejskiej kuchni !</h1>
             </div>
-            <div class="row justify-content-end">
+            <div class="row justify-content-end button-menu">
                 <button type="button" class="btn btn-light btn-lg mt-3">Menu</button>
             </div>
         </div>
     </div>
 
-    <p style="color: white;">
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-    </p>
+
+    <div class="container pb-4">
+        <div class="row icon-row">
+            <div class="col-sm text-center">
+                <i class="fas fa-thumbtack fa-5x " style="color:#dbaf1d;"></i>
+                <h3 class="icon-title">Nasza lokalizacja</h3>
+                <p class="icon-text">Restaracja Jedzeniowo <br> Katowice 40-217 <br> Staromiejska 12 </p>
+            </div>
+            <div class="col-sm text-center">
+                <i class="fas fa-mobile-alt fa-5x " style="color:#dbaf1d;"></i>
+                <h3 class="icon-title">Zadzwoń do nas</h3>
+                <p class="icon-text">+48 504 432 100 </p>
+            </div>
+            <div class="col-sm text-center">
+                <i class="far fa-clock fa-5x " style="color:#dbaf1d;"></i>
+                <h3 class="icon-title">Godziny otwarcia</h3>
+                <p class="icon-text">Od poniedziałku do niedzieli <br> 9:00 - 23:00 </p>
+            </div>
+            <div class="col-sm text-center">
+                <i class="far fa-calendar-alt fa-5x " style="color:#dbaf1d;"></i>
+                <h3 class="icon-title">Zarezerwuj stolik</h3>
+                <button type="button" class="btn btn-outline-light mt-4 mt-4">Rezerwuj</button>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col align-self-center div1 text-center">
+                <h1 class="font-color1 pb-4">Poznaj naszą restauracje!</h1>
+                <p class="font-color1 text">
+                    W 2015 powstało miejsce z pasji do designu oraz przyjemności jedzenia. Szef kuchni Marcin Bródka serwuje dania dbając o najbardziej wyrafinowane podniebienia,
+                    a swój kunszt potwierdza zdobywając np. Złotą Chochlę oraz Luksusowa Marka Roku 2016. <br>
+                    <br>
+                    W Jedzeniowo serwowane są dania szeroko pojętej kuchni europejskiej. U nas zamówić można różnorodne dania, które zaspokoją każde podniebienie.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col align-self-center div1 text-center">
+                <h1 class="font-color1 pb-4">Sprawdź nasze menu !</h1>
+                <h4 class="font-color2">Najpopularniejsze potrwy</h4>
+                <div class="row" style="padding-top: 30px; font-size:20px">
+                    <div class="col-lg-3 col-md-12 font-color1 pb-3"><i class="fas fa-pizza-slice fa-lg mr-3"></i> Pizza</div>
+                    <div class="col-lg-3 col-md-12 font-color1 pb-3"> <i class="fas fa-fish fa-lg mr-3"></i> Ryba</div>
+                    <div class="col-lg-3 col-md-12 font-color1 pb-3"><i class="fas fa-hamburger fa-lg mr-3"></i> Hamburger</div>
+                    <div class="col-lg-3 col-md-12 font-color1 pb-3"><i class="fas fa-egg fa-lg mr-3"></i> Jajecznica</div>
+                </div>
+                <div style="padding-top: 60px; font-size:20px">
+                    <button type="button" class="btn btn-outline-light btn-lg">Zobacz więcej</button>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col text-center" style="padding-top: 60px;">
+                <h1 class="font-color1 pb-4">Zobacz naszą restauracje <i class="fas fa-arrow-down ml-2"></i></h1>
+            </div>
+
+        </div>
+    </div>
+        <div class="row">
+            <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfj1N97shQMYLG9ij5WNxW5Zw4RsBeb_CrwQ&usqp=CAU" class="w-100 shadow-1-strong rounded mb-4" />
+
+                <img src="https://images.pexels.com/photos/2878742/pexels-photo-2878742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" class="w-100 shadow-1-strong rounded mb-4" />
+            </div>
+
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="https://images7.alphacoders.com/596/thumb-1920-596343.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+
+                <img src="https://images.unsplash.com/photo-1595257841889-eca2678454e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNoZWZ8ZW58MHx8MHx8&w=1000&q=80" class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
+            </div>
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="https://images.unsplash.com/photo-1581349485608-9469926a8e5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNoZWZ8ZW58MHx8MHx8&w=1000&q=80" class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
+
+                <img src="https://ak.picdn.net/shutterstock/videos/15277867/thumb/1.jpg" class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+                <button type="button" class="btn btn-outline-light btn-lg ml-sm-3 ml-lg-0 "><i class="fas fa-arrow-down ml-2 mr-2"></i></button>
+            </div>
+
+        </div>
+
+
 </body>
 
 </html>
