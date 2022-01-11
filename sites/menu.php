abb36cf087dacd6876e5145de8ddaaf2cfba69ef
ovrael <ovrael@gmail.com>
2022-01-11 19:40:45 +0100

    Added icons in menu

--- sites/menu.php
+++ sites/menu.php
@@ -8,6 +8,9 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/styles/styles.css">
+
+
+    <script src="https://kit.fontawesome.com/e1ed59b2c0.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
@@ -97,12 +100,29 @@
                     {
                         $mealData = explode(';', $line);
                         echo '<tr>';
-                        echo '<td class="menu-table-row">' . $mealNumber . '</td>';
+                        echo '<td class="menu-table-row text-center">' . $mealNumber . '</td>';
 
-                        for ($j = 0; $j < count($mealData); $j++)
+                        for ($j = 0; $j < count($mealData) - 3; $j++)
                         {
                             echo '<td class="menu-table-row">' . $mealData[$j] . '</td>';
                         }
+
+                        echo '<td class="menu-table-row text-center">' . $mealData[count($mealData) - 3] . '</td>';
+
+                        $icon = '<i class="fas fa-minus"></i>';
+                        switch ($mealData[count($mealData) - 2])
+                        {
+                            case "o":
+                                $icon = '<i class="fas fa-pepper-hot"></i>';
+                                break;
+                            case "w":
+                                $icon = '<i class="fas fa-leaf"></i>';
+                                break;
+                        }
+                        echo '<td class="menu-table-row text-center">' . $icon . '</td>';
+                        echo '<td class="menu-table-row text-center">' . $mealData[count($mealData) - 1] . 'zł </td>';
+
+
 
                         echo '</tr>';
                         $mealNumber++;
